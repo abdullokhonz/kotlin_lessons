ba0c8a1af44bd9b1ac6d7ef4cccd9aaa41d97714
abdullokhonz <abdullokhon1206@gmail.com>
2025-07-13 15:47:14 +0500

    first commit

---
+++ Kotlin/src/main/kotlin/Test.kt
@@ -0,0 +1,21 @@
+package me.abdullokhonz.kotlin
+
+fun test() {
+    val msg: String = "Hello, world!"
+    println(msg)
+}
+
+fun firstPractice() {
+    val name = "Mary"
+    val age = 20
+    println("$name is $age years old")
+}
+
+fun secondPractice() {
+    val a: Int = 1000
+    val b: String = "log message"
+    val c: Double = 3.14
+    val d: Long = 100_000_000_000_000
+    val e: Boolean = false
+    val f: Char = '\n'
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Kotlin lessons are here.
